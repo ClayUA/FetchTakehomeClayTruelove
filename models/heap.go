ec4105113646094c7962a75c54823d8326032efe
ClaytonT45 <nctruelove@crimson.ua.edu>
2024-09-23 23:42:11 -0500

    making things easier to read in heap.go

--- models/heap.go
+++ models/heap.go
@@ -3,61 +3,63 @@ package models
 import "container/heap"
 
 // creating a heap that can store and pop the oldest transactions
+// used these docs for reference https://pkg.go.dev/container/heap#Pop
 type TransactionHeap []Transaction
 
-// lenght is required to access the minimum item
-func (h TransactionHeap) Len() int {
-	return len(h)
+// length is required to access the minimum item
+func (heap TransactionHeap) Len() int {
+	return len(heap)
 }
 
 // compare timestamps to create a min heap of timestamps
-func (h TransactionHeap) Less(i, j int) bool {
-	return h[i].ParsedTime.Before(h[j].ParsedTime)
+func (heap TransactionHeap) Less(i, j int) bool {
+	return heap[i].ParsedTime.Before(heap[j].ParsedTime)
 }
 
 // swaps the location of items
-func (h TransactionHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (heap TransactionHeap) Swap(i, j int) {
+	heap[i], heap[j] = heap[j], heap[i]
 }
 
-func (h *TransactionHeap) Push(x interface{}) {
-	*h = append(*h, x.(Transaction))
+func (heap *TransactionHeap) Push(item interface{}) {
+	*heap = append(*heap, item.(Transaction))
 }
 
 // removes oldest transaction from heap
-func (h *TransactionHeap) Pop() interface{} {
-	heap := *h
+func (heapPointer *TransactionHeap) Pop() interface{} {
+	heap := *heapPointer
 	lengthOfHeap := len(heap)
 	oldestTransaction := heap[lengthOfHeap-1]
-	*h = heap[0 : lengthOfHeap-1]
+	*heapPointer = heap[0 : lengthOfHeap-1]
 	return oldestTransaction
 }
 
 // pushes a transaction onto the heap
-func (h *TransactionHeap) AddTransaction(t Transaction) {
-	heap.Push(h, t)
+func (heapPointer *TransactionHeap) AddTransaction(currentTransaction Transaction) {
+	heap.Push(heapPointer, currentTransaction)
 }
 
 // pops the oldest transaction
-func (h *TransactionHeap) RemoveOldestTransaction() Transaction {
-	return heap.Pop(h).(Transaction)
+func (heapPointer *TransactionHeap) RemoveOldestTransaction() Transaction {
+	return heap.Pop(heapPointer).(Transaction)
 }
 
 // accesses the oldest transaction so we can compare before popping
-func (h *TransactionHeap) CheckOldestTransaction() Transaction {
-	return (*h)[0]
+func (heapPointer *TransactionHeap) CheckOldestTransaction() Transaction {
+	return (*heapPointer)[0]
 }
 
 // if oldest transaction has more points than required we need to update it without removing it
 // nothing is changed internally in the heap because timestamp remains the same
-func (h *TransactionHeap) UpdateOldestTransaction(pointsToDeduct int) {
-	oldestTransaction := &(*h)[0]
+func (heapPointer *TransactionHeap) UpdateOldestTransaction(pointsToDeduct int) {
+	oldestTransaction := &(*heapPointer)[0]
 	oldestTransaction.Points -= pointsToDeduct
 	return
 }
 
+// creating new heap
 func InitializeHeap() *TransactionHeap {
-	h := &TransactionHeap{}
-	heap.Init(h)
-	return h
+	newHeap := &TransactionHeap{}
+	heap.Init(newHeap)
+	return newHeap
 }
